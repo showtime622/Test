cb76a67ceba43a7f8bf486bdeb18790ce4be5df1
superliu <superliu622@live.cn>
2018-11-24 10:38:27 +0800

    first commit

---
+++ CloneTest/DITest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneTest
+{
+    class DITest
+    {
+        public void test(iT i)
+        {
+            i.test();
+        }
+        
+    }
+
+    public class D1 : iT
+    {
+        public string name;
+
+        public void test()
+        {
+            Console.Write("I am in d1");
+        }
+    }
+
+    public class D2 : iT
+    {
+        public string name;
+
+        public void test()
+        {
+            Console.Write("I am in d2");
+        }
+    }
+
+    public  interface iT
+    {
+        void test();
+    }
+}
---
+++ CloneTest/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("guangchao", "male");
+            Person p2 = (Person)p1.Clone();
+            Person p3 = p1.DeepCopy();
+
+
+            SalesPerson sp1 = new SalesPerson();
+            SalesPerson sp2 = sp1;
+
+            p1.name = "gc";
+            Console.WriteLine("p1 name:{0}", p1.name);
+            Console.WriteLine("p1 name:{0}", p3.name);
+            Console.WriteLine("p1 name:{0}", p3.name);
+            Console.ReadLine();
+            Object ob = new object();
+        }
+    }
+
+    public class Person : ICloneable
+    {
+        public string name;
+        public string sex;
+
+        public SalesPerson sp = new SalesPerson();
+
+        public Person(string name, string sex)
+        {
+            this.name = name;
+            this.sex = sex;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person p = (Person)this.MemberwiseClone();
+            p.name = this.name;
+            p.sex = this.sex;
+            return p;
+        }
+    }
+
+    public class SalesPerson
+    {
+
+    }
+
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private static readonly Object locker = new Object();
+        private Singleton()
+        {
+
+        }
+        private static Singleton GetInstance()
+        {
+            if(instance == null)
+            {
+                lock (locker)
+                {
+                    if(instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                }
+            }
+
+            return instance;
+
+        }
+    }
+}
